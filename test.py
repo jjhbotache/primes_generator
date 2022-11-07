e6d8f06bcebf835906d88b5daae56619f2a423c1
juan <jjhuertasbotache@gmail.com>
2022-11-06 20:48:55 -0500

    one conditional added

--- test.py
+++ test.py
@@ -12,8 +12,8 @@ and it repeat until the final
 """
 
 
-primes = [1]
-no_primes= []
+
+
 
 #first, verifies if it's a number, and then runs the generate fuction
 def verify():
@@ -27,21 +27,32 @@ def verify():
 
 #Here happens the magic of the generation of the primes numbers
 def generate(until):
-    global viterations_text
+    global iterations_text
     iteration_var = int(iterations_text.get())
 
-    primes=[1]
+    primes=[]
+    no_primes= []
     current_num=2
+    already_multiplied = [[],[]]
     while current_num <= until :                            #while the current num is less or equal than the until number...
         iteration_var+=1
+        print(f"checking current number: {current_num} prime? {not(current_num in no_primes)}")
         if not(current_num in no_primes):                   #if the current number, is not in the no_primes list...    
             primes.append(current_num)                          #first of all, add it to the primes list and...
-            for x in range(until+1):                            #for the numbers (called x) from 0 to the until number given
+            max_multiplier = 2
+            
+            print(f"{current_num}*{max_multiplier} <= {until} = {((current_num*(max_multiplier))<=until)}")
+            print(f"CN in AM(1) ? = {(current_num in already_multiplied[1])} MM in AM(0) ? = {(max_multiplier in already_multiplied[0])}")
+            print(f"already multiplied? = {((current_num in already_multiplied[1])and(max_multiplier in already_multiplied[0]))}")
+            print(f"CN * MM+1 = {current_num*(max_multiplier)} in primes? {((current_num*(max_multiplier)) in no_primes)}")
+            
+            while ((current_num*max_multiplier)<=until):
                 iteration_var+=1
-                if not((current_num * x) > until):                  #if the product of the current number multiply by x is less than the until number
-                    no_primes.append(current_num*x)                     #append it to the no_primes list
-                else:                                               #if the product of the current number multiply by x is MORE than the until number
-                    break;                                              #stop cycle
+                no_primes.append(current_num*max_multiplier)
+                already_multiplied[0].append(current_num)
+                already_multiplied[1].append(max_multiplier)
+                print(f"{current_num*max_multiplier} added as a result of the CN:({current_num}) * MM:({max_multiplier})")
+                max_multiplier+=1
         current_num +=1                                     #continue with the next number
 
     #in here, we clean the entry and write the primes numbers in it
@@ -50,6 +61,10 @@ def generate(until):
         label.insert(END,(str(x)+"\n"))
 
     iterations_text.set(str(iteration_var))
+    
+    if (until == 1000) and not([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199,211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293,307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397,401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499,503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599,601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691,701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997] == primes):print("THEY ARE WRONG")
+    else: print("RIGHT")
+    
     
     
     
