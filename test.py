3b41eef7b1dced936c067f57effb50306dc0bbbb
juan <jjhuertasbotache@gmail.com>
2022-11-14 09:55:33 -0500

    less multiplications in debug

--- test.py
+++ test.py
@@ -49,6 +49,12 @@ def main_generate(until):
     global probable_primes
     
     iteration_var = 0
+    no_primes=[]
+    max_already_multiplied = 2
+    primes=[]
+    probable_primes = []
+    
+    
     probable_primes = list(range(2,until+1))                    #create a list from 2 to the final number
     for current_number in probable_primes:                      #start to go throught probable_primes list
         iteration_var+=1;print("add")                          #iteration_var used for count the iterations
@@ -66,7 +72,7 @@ def main_generate(until):
     iterations_text.set(str(iteration_var))
     
     #prints if the result actually are or not the right primes
-    if not(set([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199,211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293,307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397,401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499,503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599,601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691,701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]) == set(primes)):print("THEY ARE WRONG")
+    if not(set([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199,211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293,307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397,401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499,503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599,601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691,701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]) == set(primes)) and nums_to_generate == 1000:print("THEY ARE WRONG")
     else: print("RIGHT")
     
 
@@ -83,17 +89,27 @@ def debug(cn,max_multiplier):
     global max_already_multiplied
     global probable_primes
     
+    max_already_multiplied = 2
+    
     print (f"for {cn} deleted all the multipliers until {cn}*{max_already_multiplied}-{max_multiplier-1}")
     
+    multiplicators = list(range(max_already_multiplied,max_multiplier))
+    multipler_index = 0
+    while (multipler_index <= len(multiplicators)-1) and not((cn * multiplicators[multipler_index]) in no_primes):
+                                                                    # ... and the result is not already done  
+        
+        iteration_var+=1;print("add")                               #again, the iteration var
+        
+        no_prime = cn * multiplicators[multipler_index]                                  #the multiple of current number is saved into a variable
+        no_primes.append(no_prime)
+        try:                                                        
+            probable_primes.remove(no_prime)                        #try to remove the no_prime number
+        except:     
+            print(f"couldn't remove {no_prime}")                    #if it already isn't in there, print it
+        
+        
+        multipler_index+=1
     
-    for multiplier in range(max_already_multiplied,max_multiplier):         #from the number already multiplied to the maximun until the prodct gets bigger that the final number...                         
-                iteration_var+=1;print("add")                               #again, the iteration var
-                no_prime = cn * multiplier                                  #the multiple of current number is saved into a variable
-                try:                                                        
-                    probable_primes.remove(no_prime)                        #try to remove the no_prime number
-                except:     
-                    print(f"couldn't remove {no_prime}")                    #if it already isn't in there, print it
-     
     
 
 #------------------------------------------------------------------------------
